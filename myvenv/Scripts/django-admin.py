431a54d60a6061d26bbb588caf98d40a02294587
hyeonukdev <lego7407@gmail.com>
2019-10-26 13:53:33 +0900

    RESTFul

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lego7\desktop\navercloud\django\django-restful-api2-master\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
